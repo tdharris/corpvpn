993778094b669ca6c3e60b497ff7da54a700d348
Tyler Harris <tyler.harris@microfocus.com>
2023-12-13 09:59:44 -0700

    Fixup healthcheck

--- root/app/healthcheck.sh
+++ root/app/healthcheck.sh
@@ -12,7 +12,17 @@ services=(
     "dnsmasq"
 )
 
-for svc in "${services[@]}"; do 
+ENABLE_VPN="${1:-false}"
+ENABLE_DNS="${2:-false}"
+AUTO_RESTART_SERVICES="${3:-false}"
+
+for svc in "${services[@]}"; do
+    if [[ "$svc" == "openconnect" && ! "$ENABLE_VPN" == "true" ]]; then
+        continue
+    elif [[ "$svc" == "dnsmasq" && ! "$ENABLE_DNS" == "true" ]]; then
+        continue
+    fi
+
     if ! pgrep "$svc" >/dev/null; then
         log info "$log_prefix $svc is not running"
         if "$AUTO_RESTART_SERVICES"; then
@@ -24,7 +34,11 @@ for svc in "${services[@]}"; do
     fi
 done
 
-vpn_ip="$(ifconfig tun | grep -P -o -m 1 '(?<=inet\s)[^\s]+')"
-[[ -z "${vpn_ip}" ]] && fail "$log_prefix tun device not found!"
-ip_check="$(wget -Y on -q -O - ifconfig.co/ip)"
-echo "${ip_check}" | grep -q "${vpn_ip}" || fail "$log_prefix ip check failed - found ${ip_check} instead of ${vpn_ip}!"
+if [[ "$ENABLE_VPN" == "true" ]]; then
+    vpn_ip="$(ifconfig tun | grep -P -o -m 1 '(?<=inet\s)[^\s]+')"
+    [[ -z "${vpn_ip}" ]] && fail "$log_prefix tun device not found!"
+    ip_check="$(wget -Y on -q -O - ifconfig.co/ip)"
+    echo "${ip_check}" | grep -q "${vpn_ip}" || fail "$log_prefix ip check failed - found ${ip_check} instead of ${vpn_ip}!"
+else
+    return 0
+fi
